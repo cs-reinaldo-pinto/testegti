1a17f930008d8122ca9ff431a36c912bad77247e
cs-reinaldo-pinto <reinaldo.pinto@concrete.com.br>
2016-07-15 13:24:31 -0300

    terceiro commit.

--- youse.groovy
+++ youse.groovy
@@ -6,9 +6,7 @@ node('master'){
 	print "Estou no MASTER testAuthentication."
 
 checkout([$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '5cb777fb-b8e5-4638-8f25-0a6dc5fa0d02', url: 'git@github.com:youse-seguradora/components-authenticationi.git']]])
-
-//	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '3dc6d6aa-a9e9-44e7-b485-dc1f38601277', url: 'git@github.com:youse-seguradora/components-authentication.git']]]
-
+	
 	def nodeHome = tool name: 'Default Version (5.10.0)'
 	env.PATH="${env.PATH}:${nodeHome}/bin"
 }
@@ -21,8 +19,6 @@ node('changemore'){
 
 checkout([$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '5cb777fb-b8e5-4638-8f25-0a6dc5fa0d02', url: 'git@github.com:youse-seguradora/components-authentication.git']]])
 
-//	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '3dc6d6aa-a9e9-44e7-b485-dc1f38601277', url: 'git@github.com:youse-seguradora/components-authentication.git']]]
-	
 	sh 'cd deploy && ./start_environment.sh'	
 }
 
@@ -34,10 +30,6 @@ node('master'){
 	print "Estou no MASTER testEvents."
 
 checkout([$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '5cb777fb-b8e5-4638-8f25-0a6dc5fa0d02', url: 'git@github.com:youse-seguradora/components-ipr.git']]])
-
-// checkout([$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, recursiveSubmodules: true, reference: './shared-libs', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '3dc6d6aa-a9e9-44e7-b485-dc1f38601277', url: 'git@github.com:youse-seguradora/components-ipr.git']]])
-
-//	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '3dc6d6aa-a9e9-44e7-b485-dc1f38601277', url: 'git@github.com:youse-seguradora/components-ipr.git']]]
 
 sh '''cd "components"
 	echo "Executando os consumers."
@@ -56,8 +48,6 @@ node('changemore'){
 
 checkout([$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'SubmoduleOption', disableSubmodules: false, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '5cb777fb-b8e5-4638-8f25-0a6dc5fa0d02', url: 'git@github.com:youse-seguradora/components-ipr.git']]])
 
-//checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '3dc6d6aa-a9e9-44e7-b485-dc1f38601277', url: 'git@github.com:youse-seguradora/components-ipr.git']]]
-
 	sh 'cd deploy && ./start_environment.sh'	
 }
 
@@ -68,12 +58,9 @@ node('master'){
 	checkout scm
 	print "Estou no MASTER testNotifications."
 
-//	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '3dc6d6aa-a9e9-44e7-b485-dc1f38601277', url: 'git@github.com:youse-seguradora/components-notifications.git']]]
-
 	def nodeHome = tool name: 'Default Version (5.10.0)'
 	env.PATH="${env.PATH}:${nodeHome}/bin"
 
-//	sh 'cd "components/notifications/consumer" && npm build'
 }
 
 node('changemore'){
@@ -82,9 +69,6 @@ node('changemore'){
 	checkout scm
 	print "Estou no CHANGEMORE deployNotifications"
 
-	//checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '3dc6d6aa-a9e9-44e7-b485-dc1f38601277', url: 'git@github.com:youse-seguradora/components-notifications.git']]]
-	
-	//sh 'cd deploy && ./start_environment.sh'	
 }
 
 //====================== Manager ==========================
@@ -94,12 +78,8 @@ node('master'){
 	checkout scm
 	print "Estou no MASTER testManager."
 
-	//checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '3dc6d6aa-a9e9-44e7-b485-dc1f38601277', url: 'git@github.com:youse-seguradora/components-manager.git']]]
-
 	def nodeHome = tool name: 'Default Version (5.10.0)'
 	env.PATH="${env.PATH}:${nodeHome}/bin"
-
-	//sh 'cd "components-manager" && npm build'
 }
 
 node('changemore'){
@@ -107,9 +87,5 @@ node('changemore'){
 	stage 'deployManager'
 	checkout scm
 	print "Estou no CHANGEMORE deployManager"
-
-//	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/development']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '3dc6d6aa-a9e9-44e7-b485-dc1f38601277', url: 'git@github.com:youse-seguradora/components-manager.git']]]
-	
-	//sh 'cd deploy && ./start_environment.sh'	
 }
 
